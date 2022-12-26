ac3efc057a5d471085f0554fbd2508b215e25be7
saba akram <sabaakram211@gmail.com>
2022-12-26 13:34:49 +0530

    tabulation

--- fibonacci.java
+++ fibonacci.java
@@ -6,7 +6,8 @@ public class fibonacci {
         int n = sc.nextInt();
         int []memo = new int[n+1];
         // System.out.println(findNth_fibonacci(n));
-        System.out.println(findNth_fibonacci_memo(n,memo));
+        // System.out.println(findNth_fibonacci_memo(n,memo));
+        System.out.println(findNth_fibonacci_tabu(n));
     }
 
     // fibonacci using Recursion
@@ -29,6 +30,19 @@ public class fibonacci {
         }
         int ans=findNth_fibonacci_memo(n-1, memo) + findNth_fibonacci_memo(n-2, memo);
         return memo[n]=ans;
+    }
+
+    //using Tabulation
+    public static int findNth_fibonacci_tabu(int n){
+        int []mem = new int[n+1];
+        for(int i=0; i<=n; i++){
+            if(i==0 || i==1){
+                mem[i]=i;
+            }else{
+                mem[i]=mem[i-1]+mem[i-2];
+            }
+        }
+        return mem[n];
     }
 
 }
