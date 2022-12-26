037917844a06b6c46c6a6b538df4ec981af54c53
saba akram <sabaakram211@gmail.com>
2022-12-26 13:27:52 +0530

    memoization

--- fibonacci.java
+++ fibonacci.java
@@ -4,8 +4,9 @@ public class fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int ans = findNth_fibonacci(n);
-        System.out.println(ans);
+        int []memo = new int[n+1];
+        // System.out.println(findNth_fibonacci(n));
+        System.out.println(findNth_fibonacci_memo(n,memo));
     }
 
     // fibonacci using Recursion
@@ -16,4 +17,18 @@ public class fibonacci {
         int ans = findNth_fibonacci(n-1) + findNth_fibonacci(n-2);
         return ans;
     }
+
+    //using Memoization
+    public static int findNth_fibonacci_memo(int n,int []memo){
+        if(n==0 || n==1){
+            memo[n]=n;
+            return n;
+        }
+        if(memo[n]!=0){
+            return memo[n];
+        }
+        int ans=findNth_fibonacci_memo(n-1, memo) + findNth_fibonacci_memo(n-2, memo);
+        return memo[n]=ans;
+    }
+
 }
