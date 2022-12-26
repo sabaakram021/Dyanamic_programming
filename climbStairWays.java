8c33c6599bdd70325fe2bcacfb893a88e5e0a19a
saba akram <sabaakram211@gmail.com>
2022-12-26 15:20:00 +0530

    memoization

--- climbStairWays.java
+++ climbStairWays.java
@@ -4,7 +4,9 @@ public class climbStairWays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
-        System.out.println(findWays(n)); 
+        int []mem = new int[n+1];
+        // System.out.println(findWays(n)); 
+        System.out.println(findWays_memo(n,mem));
     }
 
     //climb stair using recursion
@@ -17,4 +19,20 @@ public class climbStairWays {
 
         return one_step+two_step+three_step;
     }
+
+    //using memoization
+    public static int findWays_memo(int n, int []mem){
+        if(n==0){
+            mem[n]=1;
+            return 1;
+        }
+        if(mem[n]!=0){
+            return mem[n];
+        }
+        int one_step=findWays(n-1);
+        int two_step=findWays(n-2);
+        int three_step=findWays(n-3);
+        return mem[n] = one_step+two_step+three_step;
+    }
+
 }
